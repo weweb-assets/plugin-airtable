81107c93981cae2656e2c0bcba2af094dd757f71
Damien DURO <damien@weweb.io>
2021-05-17 13:49:55 +0200

    rename plugin.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,112 @@
-import plugin from './plugin.js';
+/* wwEditor:start */
+import './popups';
+import { GET_AIRTABLE_BASES, GET_AIRTABLE_TABLES } from './graphql';
+/* wwEditor:end */
 
-const name = 'plugin-airtable';
+export default {
+    /* wwEditor:start */
+    /*=============================================m_ÔÔ_m=============================================\
+        Data
+    \================================================================================================*/
+    settings: {
+        data: {},
+        privateData: {
+            apiKey: '',
+            tables: [],
+        },
+    },
+    /* wwEditor:end */
+    /*=============================================m_ÔÔ_m=============================================\
+        Init
+    \================================================================================================*/
+    async init() {
+        /* wwEditor:start */
+        const plugin = wwLib.wwPlugins.pluginAirtable;
+        plugin.settings = (await wwLib.wwPlugin.getSettings(plugin.id)) || this.settings;
+        if (!plugin.settings.privateData.apiKey) plugin.settings.privateData.apiKey = '';
+        if (!plugin.settings.privateData.tables) plugin.settings.privateData.tables = [];
+        if (plugin.isNew && !plugin.settings.privateData.apiKey.length) {
+            this.sidebarButton();
+        }
+        /* wwEditor:end */
+    },
+    /* wwEditor:start */
+    /*=============================================m_ÔÔ_m=============================================\
+        META API
+    \================================================================================================*/
+    async getBases(isNoCache = false) {
+        const { data } = await wwLib.$apollo.query({
+            query: GET_AIRTABLE_BASES,
+            variables: {
+                apiKey: this.settings.privateData.apiKey,
+            },
+            fetchPolicy: isNoCache ? 'network-only' : 'cache-first',
+        });
+        return data.getAirtableBases.data;
+    },
+    async getTables(baseId, isNoCache = false) {
+        const { data } = await wwLib.$apollo.query({
+            query: GET_AIRTABLE_TABLES,
+            variables: {
+                apiKey: this.settings.privateData.apiKey,
+                baseId,
+            },
+            fetchPolicy: isNoCache ? 'network-only' : 'cache-first',
+        });
+        return data.getAirtableTables.data;
+    },
+    /*=============================================m_ÔÔ_m=============================================\
+        SYNCHRONIZE
+    \================================================================================================*/
+    async sync(table) {
+        try {
+            await wwLib.wwPlugin.saveCmsDataSet(
+                this.settings.id,
+                table.id,
+                table.tableName,
+                table.displayBy,
+                'Airtable'
+            );
 
-wwLib.wwPluginsLoader.add(name, plugin);
-
-export default plugin;
+            wwLib.wwNotification.open({
+                text: {
+                    en: `Table "${table.tableName}" succesfully fetched`,
+                },
+                color: 'green',
+            });
+        } catch (err) {
+            wwLib.wwNotification.open({
+                text: {
+                    en: 'An error occured, please try again later.',
+                    fr: 'Une erreur est survenue. Veuillez réessayer plus tard.',
+                },
+                color: 'red',
+            });
+            wwLib.wwLog.error(err);
+        }
+    },
+    async syncAll() {
+        for (const table of this.settings.privateData.tables) await this.sync(table);
+    },
+    /*=============================================m_ÔÔ_m=============================================\
+        SIDEBAR POPUP
+    \================================================================================================*/
+    async sidebarButton() {
+        try {
+            const { id, settings } = wwLib.wwPlugins.pluginAirtable;
+            const isSetup = !!settings.privateData.apiKey.length;
+            const isFirstTime = !settings.privateData.tables.length;
+            await wwLib.wwPopups.open({
+                firstPage: isSetup ? 'AIRTABLE_POPUP' : 'AIRTABLE_CONFIGURATION_POPUP',
+                data: {
+                    isFirstTime,
+                    pluginId: id,
+                    settings,
+                },
+            });
+        } catch (err) {
+            wwLib.wwLog.error(err);
+        }
+    },
+    /* wwEditor:end */
+};
