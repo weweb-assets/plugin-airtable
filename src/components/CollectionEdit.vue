8e8ae710f32c1a0b7ea4bce80a691045c28e85dc
Damien DURO <damien@weweb.io>
2021-06-02 15:38:17 +0200

    fix sort value

--- src/components/CollectionEdit.vue
+++ src/components/CollectionEdit.vue
@@ -201,7 +201,7 @@ export default {
         },
         addSort() {
             const sorts = _.cloneDeep(this.table.sort || []);
-            sorts.push({ field: this.tablesFieldsOptions[0].value, direction: 'asd' });
+            sorts.push({ field: this.tablesFieldsOptions[0].value, direction: 'asc' });
             this.setProp('sort', sorts);
         },
         setSortProp(index, value) {
