ba9f3975bc13f6c7ac19c3446f3b36da7e5c6cfc
Dams-d <damien.duro8@gmail.com>
2021-12-08 19:51:42 +0100

    Fix duplicate var

--- src/components/RecordUpdate.vue
+++ src/components/RecordUpdate.vue
@@ -137,9 +137,9 @@ export default {
         },
         setRecordData(key, value) {
             const data = { ...this.data, [key]: value };
-            for (const key in data) {
-                if (!this.tableFields.find(field => field.label === key)) {
-                    delete data[key];
+            for (const dataKey in data) {
+                if (!this.tableFields.find(field => field.label === dataKey)) {
+                    delete data[dataKey];
                 }
             }
             this.$emit('update:args', [this.collectionId, this.recordId, data]);
