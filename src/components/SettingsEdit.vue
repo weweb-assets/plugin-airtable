3d1b312b25d5f5ce36f7c3aabe237187bdf8d917
Alexis JC <alexisjc.dev@gmail.com>
2023-02-07 17:40:55 +0100

    feat: use password field type for api key

--- src/components/SettingsEdit.vue
+++ src/components/SettingsEdit.vue
@@ -6,20 +6,23 @@
                     Find it here
                 </a>
             </template>
-            <wwEditorInputText
-                type="text"
-                name="api-key"
-                placeholder="key**************"
-                :model-value="settings.privateData.apiKey"
-                :style="{ '-webkit-text-security': isKeyVisible ? 'none' : 'disc' }"
-                large
-                @update:modelValue="changeApiKey"
-            />
+            <div class="airtable-settings-edit__password">
+                <wwEditorInputText
+                    :type="isKeyVisible ? 'text' : 'password'"
+                    name="api-key"
+                    placeholder="key**************"
+                    :model-value="settings.privateData.apiKey"
+                    :style="{ '-webkit-text-security': isKeyVisible ? 'none' : 'disc' }"
+                    large
+                    @update:modelValue="changeApiKey"
+                />
+                <wwEditorIcon
+                    class="toggle"
+                    :name="isKeyVisible ? 'eye-off' : 'eye'"
+                    @click="isKeyVisible = !isKeyVisible"
+                ></wwEditorIcon>
+            </div>
         </wwEditorFormRow>
-        <div class="airtable-settings-edit__row">
-            <wwEditorInputSwitch v-model="isKeyVisible" />
-            <span class="airtable-settings-edit__radio-label caption-m">Show api key</span>
-        </div>
     </div>
 </template>
 
@@ -49,6 +52,17 @@ export default {
     &__link {
         color: var(--ww-color-blue-500);
         margin-left: var(--ww-spacing-02);
+    }
+    &__password {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        input {
+            flex: auto;
+        }
+        .toggle {
+            cursor: pointer;
+        }
     }
     &__row {
         display: flex;
