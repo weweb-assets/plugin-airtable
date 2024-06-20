abbc8761974a5ce97c2f0f453750d777ee090b99
luka-weweb <139866429+luka-weweb@users.noreply.github.com>
2024-06-20 10:56:21 +0200

    Update CollectionEdit.vue

--- src/components/CollectionEdit.vue
+++ src/components/CollectionEdit.vue
@@ -240,7 +240,7 @@ export default {
     display: flex;
     flex-direction: column;
     &__link {
-        color: var(--ww-color-blue-500);
+        color: var(--ww-color-content-brand);
         margin-left: var(--ww-spacing-02);
     }
     &__row {
